d9c0d0b39748094a55302b9e4fe98ad4a39b6205
Anita Migacheva <anitamigacheva@gmail.com>
2020-10-13 22:32:36 +0200

    hrr

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get 'promo/index'
   get 'welcome/index'
+  get 'promo', to: 'promo#index'
+  get 'about', to: 'about#index'
+
+  root 'promo#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
