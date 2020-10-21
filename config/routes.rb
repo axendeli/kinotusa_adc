0f5c928cd7d5e87297a6f949c21d34091eee40a6
Anita Migacheva <anitamigacheva@gmail.com>
2020-10-21 17:51:54 +0200

    починила структуру, сделал посты

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'about/index'
   get 'promo/index'
-  get 'welcome/index'
+
+  get 'posts/index'
+  root 'posts#index'
+
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
 
-  root 'promo#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :subscribers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_141358) do
+ActiveRecord::Schema.define(version: 2020_10_21_153411) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "subscribers", force: :cascade do |t|
     t.string "email"
@@ -18,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_10_14_141358) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
