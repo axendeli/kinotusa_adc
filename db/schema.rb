dbd79daa3f2c8d3288653fa0f692959b6f00b323
Anita Migacheva <anitamigacheva@gmail.com>
2020-12-30 18:10:56 +0200

    -

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 2020_12_30_122628) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
 end
