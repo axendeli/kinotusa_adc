351ef8f272102e62f0bd87060ecc9caca48cbd29
Anita Migacheva <anitamigacheva@gmail.com>
2021-02-11 20:12:18 +0200

    -

---
+++ app/javascript/components/A_Btn_Primary.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class A_Btn_Primary extends React.Component {
+  render () {
+    return (
+      <div ClassName="A_Btn_Primary">
+      <div>
+        КНОПКА
+      </div>
+        <A_Btn_Primary />
+      </div>
+    );
+  }
+}
+
+export default A_Btn_Primary
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,49 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+  resources :post_types
+  resources :follows
   resources :categories
-  devise_for :users
-  resources :posts do
-  resources :comments
-  end
-  get 'about/index'
-  get 'promo/index'
-
-
+  # resources :categories
+  mount Ckeditor::Engine => '/ckeditor'
+  devise_for :users, :controllers => { registration: 'registration' }
+  resources :users
+  post 'users/:id/edit', to: "users#update"
+  post 'users/:id/delete', to: "users#delite"
+  post 'users/:id/follow', to: "users#follow", as: "follow_user"
+  post 'users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+  # get 'about/index'
+  # get 'promo/index'
+resources :posts do
+    collection do
+      get :newshit
+      get :newarticles
+      get :newnews
+      get :newpuzzles
+      get :newreviews
+      get :newdiscussion
+      get :newportfolio
+      get :newevents
+    end
+    resources :answers
+    resources :comments
+    resources :likes
+    resources :favorites
+end
+  resources :subscribers
   get 'posts/index'
-  root 'promo#index'
+
 
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
+  get 'admin_context', to: 'admin_context#index'
+  root 'promo#index'
 
+  get 'users', to: 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
+  delete 'users/:id', to: 'users#destroy'
+  get 'users/:id/following', :to => "users#following", :as => :following
 
-
+  # delete 'comments/:id', to: 'comments#destroy'
+  # root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :subscribers
 end
