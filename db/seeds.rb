d82ca19999650f481e9b9eef87b4efbc714d9ce0
Anita Migacheva <anitamigacheva@gmail.com>
2021-02-15 17:26:20 +0200

    -

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.destroy_all
+Category.destroy_all
 
 puts "Destroyed everything you touch"
