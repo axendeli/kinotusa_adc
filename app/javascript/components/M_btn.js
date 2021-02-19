6e126484ab8bfb25edf6baf9c7db249edf1c2c71
Anita Migacheva <anitamigacheva@gmail.com>
2021-02-19 23:23:16 +0200

    cделала компонент кнопку

---
+++ app/javascript/components/M_btn.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class M_btn extends React.Component {
+  render () {
+    return (
+
+        <button className={'M_btn_primary'}>
+          КНОПКА
+        </button>
+
+    );
+  }
+}
+
+export default M_btn
